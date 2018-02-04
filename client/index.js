93e9b030c2a27f04d326f984b8795da1f39466bc
jessica draper <j.b.draper@hotmail.com>
2018-02-04 18:28:43 +1300

    Working on localhost

---
+++ client/index.js
@@ -0,0 +1,12 @@
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+import App from './App'
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+      <App />,
+      document.getElementById('app')
+    )
+  })

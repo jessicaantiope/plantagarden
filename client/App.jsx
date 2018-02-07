a7cab3bd3e170b0d2a98ff0d7c6f5e3114d93a0b
jessica draper <j.b.draper@hotmail.com>
2018-02-07 22:39:48 +1300

    changed font heading and made a square route

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
+
+import Square from './components/Square'
 
 const App = () => {
     return (
-           <p>This is a gardening app</p>
+        <Router>
+            <div id="app">
+                <div className="title">
+                    <h1>Plant a Garden</h1>
+                </div>
+                <div className="columns">
+                    <div className="column" id="aboutApp">
+                        <h3 class="subtitle">About</h3>
+                            <p>Welcome to Plant a Garden.  This is a space for you to plan and create your own vegetable garden.</p>
+                    </div>
+                    <div className="column">
+                        <a href="/#/square" className="button is-link is-rounded" id="createApp">Create your own garden</a>
+                        <Route path="/square" component={Square} />
+                    </div>
+                    <div className="column" id="gardenPictures">
+                        <figure className="image is-250x250">
+                            <img src="images/collection.jpg"></img>
+                        </figure>
+                    </div>
+                </div>
+            </div>
+        </Router>
     )
 }
 
